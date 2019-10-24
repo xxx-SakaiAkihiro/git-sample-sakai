7e65817f0ff55813fb0f08d624033133f37e3076
SakaiAkihiro <akihiro.sakai@rakus-partners.co.jp>
2019-10-24 13:09:29 +0900

    プロジェクトの新規作成

---
+++ src/Car.java
@@ -0,0 +1,21 @@
+
+public class Car {
+
+	public static void main(String[] args) {
+
+		int speed;
+		int gas;
+
+		
+	}
+
+	public Car() {
+	}
+
+	@Override
+	public String toString() {
+		return "Car [getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+				+ "]";
+	}
+
+}
